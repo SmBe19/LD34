3d05032da4a10498ae98a09c96a742d44086a830
SmBe19 <bsgame27@gmail.com>
2015-12-11 15:56:59 +0100

    Add empty project

---
+++ android/src/com/smeanox/games/ld34/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.smeanox.games.ld34.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.smeanox.games.ld34.LD34;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LD34(), config);
+	}
+}
