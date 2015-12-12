28dd04b259a36712dccb9ee0eecf8043e407c608
SmBe19 <bsgame27@gmail.com>
2015-12-12 20:13:23 +0100

    Add PlantFactory.

--- core/src/com/smeanox/games/ld34/world/PlantFactory.java
+++ core/src/com/smeanox/games/ld34/world/PlantFactory.java
@@ -4,4 +4,7 @@ package com.smeanox.games.ld34.world;
  * Comment
  */
 public class PlantFactory {
+	public Plant getRandomPlant(World world, float x0, float y0, float targetHeight){
+		return new Vine(world, x0, y0, targetHeight);
+	}
 }
