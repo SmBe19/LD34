8e3a1e39dd32d1dc903e77a52dd59cfcaec9ef9f
MatteoS <matteo.signer@gmail.com>
2015-12-13 06:56:42 +0100

    Add enemy texture

--- core/src/com/smeanox/games/ld34/Textures.java
+++ core/src/com/smeanox/games/ld34/Textures.java
@@ -19,6 +19,7 @@ public class Textures {
 	public Texture particle;
 	public Texture vine;
 	public Texture thorn;
+	public Texture enemy;
 
 	private Textures(){
 		manager = new AssetManager();
@@ -41,6 +42,7 @@ public class Textures {
 		manager.load("textures/particle.png", Texture.class);
 		manager.load("textures/vine.png", Texture.class);
 		manager.load("textures/thornbush.png", Texture.class);
+		manager.load("textures/enemy.png", Texture.class);
 	}
 
 	public boolean update(){
@@ -63,6 +65,7 @@ public class Textures {
 		particle = manager.get("textures/particle.png", Texture.class);
 		vine = manager.get("textures/vine.png", Texture.class);
 		thorn = manager.get("textures/thornbush.png", Texture.class);
+		enemy = manager.get("textures/enemy.png", Texture.class);
 	}
 
 	public void clear(){
