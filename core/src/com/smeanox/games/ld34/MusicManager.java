5add239031765c1039a64def59b90c707810e23d
DeinFreund <deinbestrfreund@gmail.com>
2015-12-15 01:34:53 +0100

    mixerplant

--- core/src/com/smeanox/games/ld34/MusicManager.java
+++ core/src/com/smeanox/games/ld34/MusicManager.java
@@ -10,17 +10,16 @@ import java.util.ArrayList;
 /**
  * Comment
  */
-public class MusicManager implements Updatable {
+public class MusicManager implements Updatable, Music.OnCompletionListener {
 	private static MusicManager singleton;
 
-	private ArrayList<Music> songs;
-	private Music activeSong;
+	private Music bass, sawbass1, sawbass2, sawbassarp, dance_arpeggio, dance_slow, hihat, jungle, keys,
+			kick,lead_end,lead_loop,lead_opening,pad_snow1,pad_snow_high, bassgroove;
 
 	private boolean playing;
+	private int cycle = 8046;
 
 	private MusicManager(){
-		songs = new ArrayList<Music>();
-		activeSong = null;
 		playing = !Consts.IS_DEFAULT_MUTED;
 	}
 
@@ -32,11 +31,98 @@ public class MusicManager implements Updatable {
 	}
 
 	public void load(){
-		songs.add(Gdx.audio.newMusic(Gdx.files.internal("music/blub3.0.mp3")));
+		bass = (Gdx.audio.newMusic(Gdx.files.internal("music/samples/bass.wav")));
+		sawbass1 = (Gdx.audio.newMusic(Gdx.files.internal("music/samples/saw_bass1.wav")));
+		sawbass2 = (Gdx.audio.newMusic(Gdx.files.internal("music/samples/saw_bass2.wav")));
+		sawbassarp = (Gdx.audio.newMusic(Gdx.files.internal("music/samples/saw_bass_arpeggio.wav")));
+		bass = (Gdx.audio.newMusic(Gdx.files.internal("music/samples/bass.wav")));
+		bassgroove = (Gdx.audio.newMusic(Gdx.files.internal("music/samples/bassgroove.wav")));
+		dance_arpeggio = (Gdx.audio.newMusic(Gdx.files.internal("music/samples/dance_argpeggio.wav")));
+		dance_slow= (Gdx.audio.newMusic(Gdx.files.internal("music/samples/dance_slow.wav")));
+		hihat= (Gdx.audio.newMusic(Gdx.files.internal("music/samples/hihat.wav")));
+		jungle= (Gdx.audio.newMusic(Gdx.files.internal("music/samples/jungle.wav")));
+		keys= (Gdx.audio.newMusic(Gdx.files.internal("music/samples/keys.wav")));
+		kick= (Gdx.audio.newMusic(Gdx.files.internal("music/samples/kick.wav")));
+		lead_end= (Gdx.audio.newMusic(Gdx.files.internal("music/samples/lead_end.wav")));
+		lead_loop= (Gdx.audio.newMusic(Gdx.files.internal("music/samples/lead_loop.wav")));
+		lead_opening= (Gdx.audio.newMusic(Gdx.files.internal("music/samples/lead_opening.wav")));
+		pad_snow1= (Gdx.audio.newMusic(Gdx.files.internal("music/samples/pad_snow1.wav")));
+		pad_snow_high= (Gdx.audio.newMusic(Gdx.files.internal("music/samples/pad_snow_high.wav")));
 
-		for(Music song : songs){
-			song.setVolume(Consts.SOUND_MUSIC_VOLUME);
-		}
+		bass.setOnCompletionListener(this);
+		bass.play();
+		sawbass1.play();
+		sawbass2.play();
+		sawbassarp.play();
+		bass.play();
+		bassgroove.play();
+		dance_arpeggio .play();
+		dance_slow.play();
+		hihat.play();
+		jungle.play();
+		keys.play();
+		kick.play();
+		lead_end.play();
+		lead_loop.play();
+		lead_opening.play();
+		pad_snow1.play();
+		pad_snow_high.play();
+		/*
+		bass.pause();
+		sawbass1.pause();
+		sawbass2.pause();
+		sawbassarp.pause();
+		bass.pause();
+		bassgroove.pause();
+		dance_arpeggio .pause();
+		dance_slow.pause();
+		hihat.pause();
+		jungle.pause();
+		keys.pause();
+		kick.pause();
+		lead_end.pause();
+		lead_loop.pause();
+		lead_opening.pause();
+		pad_snow1.pause();
+		pad_snow_high.pause();*/
+
+		bass.setLooping(true);
+		sawbass1.setLooping(true);
+		sawbass2.setLooping(true);
+		sawbassarp.setLooping(true);
+		bass.setLooping(true);
+		bassgroove.setLooping(true);
+		dance_arpeggio .setLooping(true);
+		dance_slow.setLooping(true);
+		hihat.setLooping(true);
+		jungle.setLooping(true);
+		keys.setLooping(true);
+		kick.setLooping(true);
+		lead_end.setLooping(true);
+		lead_loop.setLooping(true);
+		lead_opening.setLooping(true);
+		pad_snow1.setLooping(true);
+		pad_snow_high.setLooping(true);
+
+		sawbass1.setPosition(0);
+		sawbass2.setPosition(0);
+		sawbassarp.setPosition(0);
+		bass.setPosition(0);
+		bassgroove.setPosition(0);
+		dance_arpeggio .setPosition(0);
+		dance_slow.setPosition(0);
+		hihat.setPosition(0);
+		jungle.setPosition(0);
+		keys.setPosition(0);
+		kick.setPosition(0);
+		lead_end.setPosition(0);
+		lead_loop.setPosition(0);
+		lead_opening.setPosition(0);
+		pad_snow1.setPosition(0);
+		pad_snow_high.setPosition(0);
+		onCompletion(bass);
+
+		//song.setVolume(Consts.SOUND_MUSIC_VOLUME);
 	}
 
 	public boolean isPlaying() {
@@ -45,37 +131,126 @@ public class MusicManager implements Updatable {
 
 	public void setPlaying(boolean playing) {
 		this.playing = playing;
-		if (activeSong != null) {
-			if (playing) {
-				activeSong.play();
-			} else {
-				activeSong.pause();
-			}
-		}
 	}
+
+	long nextCycle = -1;
+	float lastPos = 1;
 
 	@Override
 	public void update(float delta) {
+		//if (nextCycle == -1) nextCycle = System.currentTimeMillis();
+		if (bass.getPosition() < lastPos){
+			//nextCycle += cycle;
+			onCompletion(bass);
+		}
+		lastPos = bass.getPosition();
+
+		if (this.playing && bass.getVolume() < Consts.SOUND_MUSIC_VOLUME) onCompletion(bass);
+	}
+
+	public void playNextSong(){
+	}
+
+	public void dispose(){
+	}
+
+	@Override
+	public void onCompletion(Music music) {
+/*
+		sawbass1.pause();
+		sawbass2.pause();
+		sawbassarp.pause();
+		bass.pause();
+		bassgroove.pause();
+		dance_arpeggio .pause();
+		dance_slow.pause();
+		hihat.pause();
+		jungle.pause();
+		keys.pause();
+		kick.pause();
+		lead_end.pause();
+		lead_loop.pause();
+		lead_opening.pause();
+		pad_snow1.pause();
+		pad_snow_high.pause();*/
+		System.out.println("playing");
+		bass.setVolume(0);
+		sawbass1.setVolume(0);
+		sawbass2.setVolume(0);
+		sawbassarp.setVolume(0);
+		bass.setVolume(0);
+		bassgroove.setVolume(0);
+		dance_arpeggio .setVolume(0);
+		dance_slow.setVolume(0);
+		hihat.setVolume(0);
+		jungle.setVolume(0);
+		keys.setVolume(0);
+		kick.setVolume(0);
+		lead_end.setVolume(0);
+		lead_loop.setVolume(0);
+		lead_opening.setVolume(0);
+		pad_snow1.setVolume(0);
+		pad_snow_high.setVolume(0);
 		if(!playing){
 			return;
 		}
 
-		if(activeSong == null || !activeSong.isPlaying()){
-			playNextSong();
-		}
-	}
+		float  vol = Consts.SOUND_MUSIC_VOLUME;
+		bass.setVolume(vol);
 
-	public void playNextSong(){
-		if(activeSong != null){
-			activeSong.stop();
-		}
-		activeSong = songs.get(MathUtils.random(songs.size() - 1));
-		activeSong.play();
-	}
 
-	public void dispose(){
-		for(Music song : songs){
-			song.dispose();
+		int rnd = MathUtils.random(0,10);
+		if (rnd < 7){
+			sawbass1.setVolume(vol);
+		}else if (rnd < 9){
+			sawbass2.setVolume(vol);
+		}else if (rnd < 11){
+			sawbassarp.setVolume(vol);
 		}
+
+		rnd = MathUtils.random(0,10);
+
+		if (rnd < 7){
+			dance_slow.setVolume(vol);
+		}else if (rnd < 11){
+			dance_arpeggio.setVolume(vol);
+		}
+
+
+		rnd = MathUtils.random(0,10);
+
+		if (rnd < 9){
+			bassgroove.setVolume(vol);
+		}else if (rnd < 11){
+		}
+		rnd = MathUtils.random(0,10);
+
+		if (rnd < 9){
+			keys.setVolume(vol);
+		}else if (rnd < 11){
+		}
+
+		hihat.setVolume(0.1f);
+
+		rnd = MathUtils.random(0,10);
+		if (rnd < 7){
+			kick.setVolume(vol);
+		}else if (rnd < 11){
+		}
+
+		rnd = MathUtils.random(0,10);
+		if (rnd < 4){
+			jungle.setVolume(vol);
+		}else if (rnd < 11){
+		}
+
+		rnd = MathUtils.random(0,10);
+		if (rnd < 7){
+			pad_snow1.setVolume(vol);
+		}else if (rnd < 11){
+			pad_snow_high.setVolume(vol);
+		}
+
+
 	}
 }
